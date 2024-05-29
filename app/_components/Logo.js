75339ce88655bdfccf1a89340b44afa9510e19d7
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-05-29 22:11:37 +0100

    set up tailwind and custom styles for primary and accent colours. Tested tailwind by adding some test styling to logo.js and layout.js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -2,9 +2,11 @@ import Link from 'next/link';
 
 function Logo() {
   return (
-    <Link href="/">
+    <Link href="/" className="flex items-center gap-4 z-10">
       <img src="/logo.png" width="60" alt="Tranquil Waters Logo" />
-      <span>Trainquil Waters</span>
+      <span className="text-xl font-semibold text-primary-100">
+        Tranquil Waters
+      </span>
     </Link>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,17 @@
+import '@/app/_styles/globals.css';
+
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 
 export const metadata = {
   title: 'Tranquil Waters',
-  description: 'Website for Trainquil Waters; a cabin lodge',
+  description: 'Website for Tranquil Waters; a cabin lodge',
 };
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen bg-primary-950 text-primary-100">
         <header>
           <Logo />
           <Navigation />
