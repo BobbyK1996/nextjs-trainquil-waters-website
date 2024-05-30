a71ec2d4957d6bda8b4d52cbcc4ae2765efabe57
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-05-30 02:11:43 +0100

    used Next.js Image component and learned its optimisation techniques

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '@/public/logo.png';
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" width="60" alt="Tranquil Waters Logo" />
+    <Link href="/" className="z-10 flex items-center gap-4">
+      {/* <Image
+        src="/logo.png"
+        width="60"
+        height="60"
+        alt="Tranquil Waters Logo"
+      /> */}
+      <Image src={logo} width="60" quality={100} alt="Tranquil Waters Logo" />
       <span className="text-xl font-semibold text-primary-100">
         Tranquil Waters
       </span>
