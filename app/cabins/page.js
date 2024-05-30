a1c728f5880c4fc8874eaa7c3f5cc2c5bcf72a0d
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-05-30 15:30:01 +0100

    Created a static page for cabins and about. About is created as per the style guide

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -5,7 +5,17 @@ export const metadata = {
 function Page() {
   return (
     <div>
-      <h1>Cabins</h1>
+      <h1 className="mb-5 text-4xl font-medium text-accent-400">
+        Our Luxury Cabins
+      </h1>
+      <p className="mb-10 text-lg text-primary-200">
+        Cozy yet luxurious cabins, located right in the heart of the Pyrenees.
+        Imagine waking up to breathtaking mountain views, spending your days
+        exploring the verdant forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to peace.
+      </p>
     </div>
   );
 }
