da2471a77d5e965747c98dc7454a81da722f722e
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-06-04 00:34:28 +0100

    Big commit. Created the layout and sidebar for Guest Area under account. Also created the structure for reservations page, with bookings=[] being a placeholder to begin with.

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,5 +1,34 @@
-function page() {
-  return <div></div>;
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Reservations',
+};
+
+function Page() {
+  const bookings = [];
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold text-accent-400 mb-7">
+        Your reservations
+      </h2>
+
+      {bookings.length === 0 ? (
+        <p className="text-lg">
+          You have no reservations yet. Check out our{' '}
+          <Link className="underline text-accent-500" href="/cabins">
+            luxury cabins
+          </Link>
+        </p>
+      ) : (
+        <ul className="space-y-6">
+          {bookings.map((booking) => (
+            <div key={booking.id}>Reservation Card Placeholder</div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default page;
+export default Page;
