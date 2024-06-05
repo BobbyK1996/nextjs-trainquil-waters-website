8f8af2bbb9ad478391c05dc957759ab9b07f419f
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-06-05 14:53:52 +0100

    created a supabase.js and stored url and key in an env. Added helper functions to data-service.js to pull data from supabase

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -9,3 +9,45 @@ export async function getCountries() {
     throw new Error('Could not fetch countries');
   }
 }
+
+export async function getCabin(id) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+}
+
+export async function getCabinPrice(id) {
+  const { data, error } = await supabase
+    .from('cabins')
+    .select('regularPrice, discount')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+}
+
+export async function getCabins() {
+  const { data, error } = await supabase
+    .from('cabins')
+    .select('id, name, maxCapacity, regularPrice, discount, image')
+    .order('name');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
+  }
+
+  return data;
+}
